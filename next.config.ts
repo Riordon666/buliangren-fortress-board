f12df5f8046e90fe16e3f97127c261ac7985810d
Riordon666 <2717831140@qq.com>
2026-08-09 12:32:17 +0800

    build: reduce memory usage on small servers

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: "standalone",
   devIndicators: false,
   experimental: {
+    webpackMemoryOptimizations: true,
+    preloadEntriesOnStart: false,
     serverActions: {
       bodySizeLimit: "3mb"
     }
